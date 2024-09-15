a30ea85afb6717de7e401c8acbe8b58a35ec0608
FreytagDom <dominicfreytag@web.de>
2024-09-15 18:48:46 +0200

    add all inkl output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+ 
+  distDir: 'all-inkl',
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
