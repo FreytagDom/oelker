f02fe31e02ae17d27531ca2f62eb2cf4f2d29750
FreytagDom <dominicfreytag@web.de>
2024-09-25 12:49:50 +0200

    add seo

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,16 @@ export default function Home() {
         <title>Karl-Christian Oelker</title>
         <meta name="description" content="Website von Karl-Christian Oelker" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Etwas
+          Romantik, etwas Sehnsucht und immer wieder ein Nachdenken über den
+          eigenen Weg zum Glück und zur Selbsterkenntnis."
+        />
+        <meta
+          name="description"
+          content="Lebenslinien, Klopfzeichen, Homos Technicus, Nachkommen"
+        />
       </Head>
       <Homepage />
     </>
